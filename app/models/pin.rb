332e53af58274e5f654b7198c074048ae1b3715a
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2016-08-25 03:14:37 +0000

    changed pin.rb...

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,4 @@
-class Pin < ActiveRecord::Base
-    belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    :default_url => "/images/medium/missing.png"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+class User < ActiveRecord::Base
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
