b8bc1609a7ae2ffe8cc84be1aa09bb099c7889ec
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2016-08-29 06:36:24 +0000

    edited the nav-bar, removed turbo-links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= reqyure jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require masonry/jquery.masonry
